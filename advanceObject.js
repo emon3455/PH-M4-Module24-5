92bc89c13a93c84b7e5a47b2f2e36e0ae76a1123
emon3455 <emon.mhk69@gmail.com>
2023-02-07 23:59:16 +0600

    object

---
+++ advanceObject.js
@@ -0,0 +1,25 @@
+const student = {
+
+    name: "Emon",
+    id: 211002117,
+    gpa: [3.41, 3.75, 3.75, 3.96, 3.92, 4],
+    cgpa: 3.82,
+    skills: {
+        html: "95%",
+        css: "90%",
+        js: "80%",
+        frameWork:{
+            Bootstrap: "90%",
+            tailwindCss: "90%"
+        }
+    },
+    about: [
+        {inteligent: "80%"},
+        {locks: "75%"},
+        {hardWorking: "90%"}
+        
+    ]
+
+}
+
+console.log(student);
